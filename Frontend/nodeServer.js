43639d9fd18785dc4c72512916bc3e22c4c26d11
Mjohansen1997 <mjohansen1997@gmail.com>
2021-11-12 19:39:13 +0100

    changed the url of the frontend server

--- Frontend/nodeServer.js
+++ Frontend/nodeServer.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function (req, res) {
-    res.sendFile(path.join('/usr/src/app/', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
 });
 app.listen(3000, () => {
     console.log("server is runnig on port 3000");
