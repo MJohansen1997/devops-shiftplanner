474170ae3fe9de260a43f4d20900393995a7cdeb
Jacob <s174130@student.dtu.dk>
2021-11-13 20:38:37 +0100

    Changed some stuff

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -135,9 +135,9 @@ export const Server = async () => {
             limit: '50mb',
         })
     )
-    const port = process.env.NODE_ENV || 8080
+    const port = 8080
 
-    app.listen(port, () => console.log(`Listening on port ${port}!`))
+    app.listen(port, '0.0.0.0', () => console.log(`Listening on port ${port}!`))
 }
 
 Server()
