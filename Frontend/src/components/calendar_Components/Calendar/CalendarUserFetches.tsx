c606eeea2a60d6fe410df9c60dbbbc48b516814e
Jacob <s174130@student.dtu.dk>
2021-12-03 00:54:07 +0100

    Fixed some bongo in mads code

--- Frontend/src/components/calendar_Components/Calendar/CalendarUserFetches.tsx
+++ Frontend/src/components/calendar_Components/Calendar/CalendarUserFetches.tsx
@@ -1,16 +1,20 @@
-import axios from "axios"
-import { UserDayShift,  Shift } from '../../../../../Backend/src/Types'
-import { render, screen } from '@testing-library/react';
+import axios from 'axios'
+import { UserDayShift } from '../../../../../Backend/src/Types'
 
 export const getDayShifts = async (date: string): Promise<UserDayShift[] | undefined> => {
-   try {
-      const fetchUserShifts = (await axios.post<UserDayShift[]>(`${process.env.REACT_APP_URL}api/fetchUsersShift`, {date: date}, { withCredentials: true })).data
-      if(fetchUserShifts == undefined) {
-         console.log("data undefined")
-      }  
-      return fetchUserShifts
-   } catch (e) {
-       console.log("couldnt fetch user shifts: " + e)
-       
-   }
+    try {
+        const fetchUserShifts = (
+            await axios.post<UserDayShift[]>(
+                `${process.env.REACT_APP_URL}/api/fetchUsersShift`,
+                { date: date },
+                { withCredentials: true }
+            )
+        ).data
+        if (fetchUserShifts == undefined) {
+            console.log('data undefined')
+        }
+        return fetchUserShifts
+    } catch (e) {
+        console.log('couldnt fetch user shifts: ' + e)
+    }
 }
--- Frontend/src/components/calendar_Components/Calendar/Calendar_Day/Calender_View_Day.tsx
+++ Frontend/src/components/calendar_Components/Calendar/Calendar_Day/Calender_View_Day.tsx
@@ -1,10 +1,9 @@
-import React, {useState, useEffect} from "react";
-import { addMonths, format, isSameMonth, addDays, startOfWeek, endOfWeek, startOfMonth, endOfMonth, getWeek, getISOWeek, isSameDay  } from "date-fns";
-import {Link, useParams} from "react-router-dom";
-import Axios from 'axios'
+import { addDays, format } from 'date-fns'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { UserDayShift } from '../../../../../../Backend/src/Types'
 // import { IFruitData, User} from 'devops-shiftplanner/Backend/src/Types'
 import { getDayShifts } from '../CalendarUserFetches'
-import { UserDayShift,  Shift } from '../../../../../../Backend/src/Types'
 
 type dateyoink = {
     date: string
@@ -12,21 +11,17 @@ type dateyoink = {
 
 export const CalendarDay = () => {
     const [currentDay, setCurrentDay] = useState(new Date())
-    const [hover, setHover] = useState(false);
+    const [hover, setHover] = useState(false)
     const [shifts, setShifts] = useState<UserDayShift[] | undefined>([])
-     
-    
-    const { date } = useParams<dateyoink>();
 
-    
+    const { date } = useParams<dateyoink>()
 
     const getShifts = async () => {
         try {
-            const fetch = (await getDayShifts(date))
-            if(fetch !== undefined)
-                setShifts(fetch)
+            const fetch = await getDayShifts(date)
+            if (fetch !== undefined) setShifts(fetch)
         } catch (e) {
-            console.log("couldnt fetch user shifts: " + e)
+            console.log('couldnt fetch user shifts: ' + e)
         }
     }
 
@@ -34,16 +29,12 @@ export const CalendarDay = () => {
         getShifts()
     }, [])
 
-
-
     function nextDay() {
         setCurrentDay(addDays(currentDay, +1))
     }
     function prevDay() {
         setCurrentDay(addDays(currentDay, -1))
     }
-    
-
 
     const renderHeader = () => {
         const dateFormat = 'B..BBB'
@@ -67,50 +58,48 @@ export const CalendarDay = () => {
 
         for (let i = 0; i < 24; i++) {
             columns.push(
-                <div className="text-center text-black text-sm sm:text-base font-bold p-1 sm:p-2">
-                    {' '}
-                    {i % 24}:00{' '}
-                </div>
+                <div className="text-center text-black text-sm sm:text-base font-bold p-1 sm:p-2"> {i % 24}:00 </div>
             )
         }
-        
 
         return (
             <div className="flex flex-col max-h-screen mx-5 mt-5">
                 {renderHeader()}
                 <div className="grid grid-cols-24 grid-flow-auto divide-x-2 divide-sky-800 overflow-x-hidden overflow-y-auto gap-x-0 gap-y-2 pb-1  border border-black bg-gray-200 justify-center">
                     {columns}
-                    
+
                     {shifts ? (
-                        shifts.map(({firstname, email, shift}) => {
-                            
-                            return shift ? (shift.map(({startTime, endTime}, index) => {
-                                return (
-                                    <>
-                                        <div className={`flex flex-col border-2 border-black col-start-${startTime+1} col-span-${endTime - startTime} col-end-${endTime} p-1 bg-sky-200 hover:bg-sky-400 text-black font-bold justify-center items-center `} key={index}>
-                                            <p className="flex w-full text-sm"> {firstname} </p>
-                                            <p className="flex w-full text-sm"> {email} </p>
-                                        </div>
-                                        <div className="span-1">
-                                            
-                                        </div>
-                                    </>
+                        shifts.map(({ firstname, email, shift }) => {
+                            return shift ? (
+                                shift.map(({ startTime, endTime }, index) => {
+                                    return (
+                                        <>
+                                            <div
+                                                className={`flex flex-col border-2 border-black col-start-${
+                                                    startTime + 1
+                                                } col-span-${
+                                                    endTime - startTime
+                                                } col-end-${endTime} p-1 bg-sky-200 hover:bg-sky-400 text-black font-bold justify-center items-center `}
+                                                key={index}
+                                            >
+                                                <p className="flex w-full text-sm"> {firstname} </p>
+                                                <p className="flex w-full text-sm"> {email} </p>
+                                            </div>
+                                            <div className="span-1"></div>
+                                        </>
                                     )
-                                })) : (<div></div>)
-                                
-                            })
-                        ) : (<div> Loading elements.. </div>)
-                    }
-                    
+                                })
+                            ) : (
+                                <div></div>
+                            )
+                        })
+                    ) : (
+                        <div> Loading elements.. </div>
+                    )}
                 </div>
             </div>
         )
     }
 
-    return (
-        <div className="flex flex-col flex-none bg-white">
-            
-            {renderCells()}
-        </div>
-    )
+    return <div className="flex flex-col flex-none bg-white">{renderCells()}</div>
 }
